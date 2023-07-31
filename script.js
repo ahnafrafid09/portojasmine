88dea0708369e93965b69ae77b143f121fff2c0d
Ahnaf <Ahnafrafid22@gmail.com>
2023-07-31 21:26:59 +0700

    update

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-const sidemenu = document.getElementById("sidemenu")
-
-const openmenu = () => {
-  sidemenu.style.right = "0"
-  sidemenu.style.transition ="0.5s"
-}
-
-const closemenu = () => {
-  sidemenu.style.right = "-200px"
-}
-
-// about menu
-
 const tablinks = document.getElementsByClassName("tab-links");
 const tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
   for (tablink of tablinks) {
-    tablink.classList.remove("active-links");
+    tablink.classList.remove("active-link");
   }
   for (tabcontent of tabcontents) {
     tabcontent.classList.remove("active-tab");
   }
-  event.currentTarget.classList.add("active-links")
+  event.currentTarget.classList.add("active-link")
   document.getElementById(tabname).classList.add("active-tab")
+}
+
+const sidemenu = document.getElementById("sidemenu")
+
+const openmenu = () => {
+  sidemenu.style.right = "0"
+}
+
+const closemenu = () => {
+  sidemenu.style.right = "-200px"
 }
